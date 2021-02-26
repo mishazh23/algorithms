a8a272a3bb6d184546bc3f0b48926a747c8738ff
Mariya Zhavrid <nomoresorrow23@gmail.com>
2021-02-26 12:33:29 +0300

    add Turing machine

---
+++ TuringMachine/Run.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TuringMachine
+{
+    class Run
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string that starts and ends with '#' and includes only 'a', 'b', 'c': ");
+            string tape1 = Console.ReadLine();
+            TuringMachine machine = new TuringMachine();
+            machine.Task1();
+            char[] result1 = TuringMachine.TuringMachineProccesing(tape1.ToCharArray());
+            Console.WriteLine(result1);
+
+            Console.WriteLine("Enter string that starts and ends with '##' and includes only '1': ");
+            string tape2 = Console.ReadLine();
+            machine.Task2();
+            char[] result2 = TuringMachine.TuringMachineProccesing(tape2.ToCharArray()); 
+            Console.WriteLine(result2);
+        }
+    }
+}
---
+++ TuringMachine/TuringMachine.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace TuringMachine
+{
+    class TuringMachine
+    {
+        public static Dictionary<(string state, char symbol), (string new_state, char new_symbol, int step)> Transmissions;
+        public  void Task1()
+        {
+            Transmissions = new Dictionary<(string state, char currentSymbol), (string nextState, char newSymbol, int shift)>
+                    {
+                        {("q0", '#'), ("q0", '#', 1)},
+                        {("q0", 'a'), ("q1", 'a', 0)},
+                        {("q0", 'b'), ("q1", 'b', 0)},
+                        {("q0", 'c'), ("q1", 'c', 0)},
+
+                        {("q1", 'a'), ("q2", 'a', 1)},
+                        {("q1", 'b'), ("q1", 'c', 1)},
+                        {("q1", 'c'), ("q1", 'c', 1)},
+                        {("q1", '#'), ("qy", '#', 0)},
+
+                        {("q2", 'a'), ("q2", 'a', 1)},
+                        {("q2", 'b'), ("q2", 'b', 1)},
+                        {("q2", 'c'), ("q2", 'c', 1)},
+                        {("q2", '#'), ("q3", 'a', -1)},
+
+                        {("q3", 'a'), ("q3", '#', -1)},
+                        {("q3", 'b'), ("q3", '#', -1)},
+                        {("q3", 'c'), ("q3", '#', -1)},
+                        {("q3", '#'), ("qy", '#', 0)}
+                    };
+        }
+
+        public void Task2()
+        {
+            Transmissions = new Dictionary<(string state, char currentSymbol), (string nextState, char newSymbol, int shift)>
+                    {
+                        {("q0", '#'), ("q0", '#', 1)},
+                        {("q0", '1'), ("q1", '1', -1)},
+
+                        {("q1", '#'), ("q2", '1', 1)},
+
+                        {("q2", '1'), ("q2", '1', 1)},
+                        {("q2", '#'), ("qy", '1', 0)}
+                    };
+        }
+
+        public static char[] TuringMachineProccesing(char[] tape)
+        {
+            string state = "q0";
+            int position = 0;
+
+            while (state != "qy")
+            {
+                Transmissions.TryGetValue((state, tape[position]), out var value);
+                state = value.new_state;
+                tape[position] = value.new_symbol;
+                position += value.step;
+            }
+
+            return tape;
+        }
+
+    }
+}
